ff3800e64d95d4f5d317eb84623635ddd0951ad3
Ryan Thornburg <ryan.thornburg@unc.edu>
2020-03-02 12:20:16 -0500

    Add files via upload

---
+++ RunThisFirst.R
@@ -0,0 +1,15 @@
+
+# part 1 ------------------------------------------------------------------
+
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("scales")
+
+
+# part 2 Uncomment the line that matches your operating system---------------
+
+#Windows
+#setwd('C:/Users/your-user-name-here/Documents/MEJO570')
+
+#Mac
+#setwd('~/Documents/MEJO570')
